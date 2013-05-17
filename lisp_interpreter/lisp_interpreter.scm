3d2cb0590448f2eb85a0e6ed2debba1880818f59
Alfred Zhong <az@cs.brown.edu>
2013-05-17 12:18:40 -0400

    * implemented cond, working on the evaluator

--- lisp_interpreter/lisp_interpreter.scm
+++ lisp_interpreter/lisp_interpreter.scm
@@ -9,6 +9,9 @@
 ; * symbol?  example: (symbol? 'abc) returns #t
 
 
+; !!!! The following are helper functions !!!!
+
+
 ; if an expression is a number or a string
 ; it is self-evaluating
 (define (self-evaluating? exp)
@@ -46,6 +49,12 @@
 	(tagged-list? exp 'quote)	
 )
 
+
+
+
+
+; !!!! The following are types of lisp statements !!
+; assignment, lambda, definition, if, begin, applicaiton(function call),
 
 ;assignment
 (define (assignment? exp)
@@ -163,11 +172,95 @@
 	      ( else             (make-begin seq) )
 	)
 )
-; the return for null and 1 element is strange!!
-; like (3) () ...
-; these not legal expression
+
+
+
+
+;applicaiton, a function call like (f p1 p2 p3 p4)
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operand ops) (cdr ops))
+
+
+
+
+
+
+; !!! derived expressions !!!
+; cond
+; for example
+;	(cond ((> x 0) x)
+;	      ((= x 0) (display 'zero) 0)
+;	      (else (- x)))
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
+
+(define (cond-else-clause? exp) 
+	(eq? (cond-predicate exp) 'else)
+)
+
+(define (expand-clauses clauses)
+	(if (null? clauses)	
+		'false
+		(let ( (first (car clauses))
+		       (rest  (cdr clauses))
+		     )
+
+		     (if (cond-else-clause? first) ; is this the last statement?
+		     	(if (null? rest) ; the end of cond
+				(sequence->exp (cond-actions first)) ;if only one exp, just itself
+								     ;if null, just itself
+								     ;if multiple expressions, use begin
+				(error "Else clause itsn't last -- COND->IF")
+			)
+			(make-if (cond-predicate first) ;not end of cond yet
+				(sequence->exp (cond-actions first))
+				(expand-clauses rest)
+			)
+		     )
+		)
+	)
+)
+; question: cond-actions can be a sequence of expression without begin, doesn't seem possible
+; yes like  (cond (#f "hello") (#t (+ 3 4) (+ 2 3) ))
+
+(define (cond->if exp)
+	(expand-clauses (cond-clauses exp))
+)
+
+
+
+
+; the evaluator data structures
+(define (true? x) 
+	(not (false? x))
+)
+
+(define (false? x)
+	(eq? x #f)
+)
+
+
+
+
+;(define (ieval exp env)
+;	(cond ( (self-evaluating? exp) exp )
+;	      ( (variable? exp) (lookup-variable-value exp env) )
+;	      ( (quoted? exp) (text-of-quotation exp) )
+;	)
+;)
+
 
 
 
 ;(load "lisp_interpreter.scm")
+
 
